8052833277b4e1c838699e87ed905ef9c8ab5861
Tamir Shteinberg <sht.tamir@gmail.com>
2024-10-03 21:36:59 +0300

    git ignore update, token and sheet id hide

--- google_sheets.py
+++ google_sheets.py
@@ -2,8 +2,16 @@ import os.path
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
+# Open the file in read mode
+with open('token.txt', 'r') as file:
+    # Read the token from the file
+    token = file.read().strip()
+
+# Print the token
+print(f'Token: {token}')
+
 # Replace with your spreadsheet ID
-SPREADSHEET_ID = '1n2mTOacP_UV4zBE10xHy5d3LobNDGkNiXsvH33Trejo'
+SPREADSHEET_ID = token
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 def get_sheet_data(range_name):
